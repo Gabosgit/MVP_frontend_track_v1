f75b36871e10c8de401dff1b95489718486e219d
Gabo <no@email>
2025-06-26 10:30:40 +0200

    ADDED component ShowMoreContainer.jsx to manage long text fields (e.g. in Profile Bio)

---
+++ src/components/ShowMoreContainer.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+export default function ShowMoreContainer({text}) {
+    const [showMore, setShowMore] = useState(true)
+
+    function toggleShowMore() {
+        setShowMore(prevShowMore => !prevShowMore)
+    }
+
+    return (
+        <div>
+            <p className="text-gray-600 text-justify text-md sm:text-lg leading-relaxed overflow-hidden">
+                {!showMore ? text : text.slice(0, 300) + " ..."}
+            </p>
+            <div className="flex justify-end mt-1">
+                <button onClick={toggleShowMore} className="p-2 font-bold text-blue-700">
+                    {!showMore ? "show less" : "show more"}
+                </button>
+            </div>
+        </div>
+    )
+}
